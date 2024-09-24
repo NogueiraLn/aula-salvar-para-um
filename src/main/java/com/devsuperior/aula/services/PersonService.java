49423a03b05f918ac0453960be6d481c802ffa15
Nog <lucas.nogueiramartins@gmail.com>
2024-09-23 23:51:46 -0300

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -3,6 +3,7 @@ package com.devsuperior.aula.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -36,4 +37,24 @@ public class PersonService {
 		entity = repository.save(entity);
 		return new PersonDepartmentDTO(entity);
 	}
+	
+	public PersonDTO insert(PersonDTO dto) {
+		Person entity = new Person();
+		
+		entity.setSalary(dto.getSalary());
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+		
+//		Department dep = departmentRepository
+//				.getReferenceById(dto.getDepartmentId()); // To return Department name in JSON
+		
+		Department dep = new Department(); // Return just Department ID
+		dep.setId(dto.getDepartmentId());
+		
+		entity.setDepartment(dep);
+		
+		entity = repository.save(entity);
+		return new PersonDTO(entity);
+	}
+	
 }
